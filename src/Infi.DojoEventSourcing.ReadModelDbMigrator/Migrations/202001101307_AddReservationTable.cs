530a4b377997c0d4a17b2fc91e542258fd4e7f9f
Bryan Scheele <bryan@infi.nl>
2020-01-10 14:38:00 +0100

    Add readmodel migrator

---
+++ src/Infi.DojoEventSourcing.ReadModelDbMigrator/Migrations/202001101307_AddReservationTable.cs
@@ -0,0 +1,19 @@
+using FluentMigrator;
+
+namespace Infi.DojoEventSourcing.ReadModelDbMigrator.Migrations
+{
+    [Migration(202001101307)]
+    public class AddReservationTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Reservation")
+                .WithColumn("Id").AsGuid().PrimaryKey();
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Infi.DojoEventSourcing.ReadModelDbMigrator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using FluentMigrator.Runner;
+using Infi.DojoEventSourcing.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Infi.DojoEventSourcing.ReadModelDbMigrator
+{
+    public static class Program
+    {
+        private static readonly IConfiguration Configuration = ConfigurationFactory.Create();
+
+        public static void Main()
+        {
+            Log.Information("Started Infi.DojoEventSourcing.ReadModelDbMigrator");
+            var serviceProvider = CreateServices();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static void UpdateDatabase(IServiceProvider scopeServiceProvider)
+        {
+            scopeServiceProvider
+                .GetRequiredService<IMigrationRunner>()
+                .MigrateUp();
+        }
+
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(
+                    rb => rb
+                        .AddSQLite()
+                        .WithGlobalConnectionString(Configuration["SqlLite:ConnectionString"])
+                        .ScanIn(Assembly.GetExecutingAssembly())
+                        .For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+    }
+}
